42833b250d23a61ef6542c4866b52f4e546dc6a2
AL Gzl <algzl1727@gmail.com>
2018-05-31 22:12:18 +0700

    Add files via upload

---
+++ Database/projectuas.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2018 at 04:59 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projectuas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `identitas`
+--
+
+CREATE TABLE `identitas` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `status` varchar(15) DEFAULT NULL,
+  `jurusan` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `identitas`
+--
+
+INSERT INTO `identitas` (`id`, `nama`, `status`, `jurusan`) VALUES
+(1, 'AL Ghozaly', 'Mahasiswa', 'Informatika'),
+(2, 'Fitri Sintadewi', 'Mahasiswa', 'Keperawatan'),
+(6, 'Lyandika Putra', 'Dosen', 'Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `identitas`
+--
+ALTER TABLE `identitas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `identitas`
+--
+ALTER TABLE `identitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ README.md
@@ -0,0 +1,24 @@
+# Project-UAS
+# Documentasi Selama Praktikum sebelum UAS
+
+## Tampilan Awal
+
+![1](https://github.com/algzl17/Project-UAS/blob/master/screenshot/1.png)
+
+## Tampilan Source Code dan Database
+- Source Code
+![prev](https://github.com/algzl17/Project-UAS/blob/master/screenshot/7.png)
+- Database
+![prev](https://github.com/algzl17/Project-UAS/blob/master/screenshot/2.png)
+
+## Tampilan Running Program
+
+1. Tampilan Data-Data Mahasiswa
+![2](https://github.com/algzl17/Project-UAS/blob/master/screenshot/3.png) <br>
+2. Tampilan Ketika Input Data Mahasiswa
+![3](https://github.com/algzl17/Project-UAS/blob/master/screenshot/4.png)<br>
+3. Tampilan Ketika Edit Data Mahasiswa
+![4](https://github.com/algzl17/Project-UAS/blob/master/screenshot/5.png)<br>
+4. Tampilan Hasil Setelah Data "Naufal Islami Putra" berhasil di hapus
+![5](https://github.com/algzl17/Project-UAS/blob/master/screenshot/6.png)<br>
+
